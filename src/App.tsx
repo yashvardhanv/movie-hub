bd6d850c865692a99e6235dafe17f400ac541961
yashvardhanv <yashji2117@gmail.com>
2023-11-25 11:56:56 +0530

    updating

--- src/App.tsx
+++ src/App.tsx
@@ -5,46 +5,51 @@ import GenreList from "./componants/GenreList"
 import { useState } from "react"
 import { Genre } from "./hooks/useGenres"
 import PlateformSelector from "./componants/PlateformSelector"
-import { Item } from "./componants/PlateformSelector" 
+import { Item } from "./componants/PlateformSelector"
 import PageDirection from "./componants/PageDirection"
+import { Analytics } from '@vercel/analytics/react';
 
-function App()
-{
+function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   const [selectedFilter, setSelectedFilter] = useState<Item | null>(null);
   const [searchText, setSearch] = useState("")
   const [page, setPage] = useState(1);
 
 
-  const l = [{name:null, value: null},{name:"Popularity", value:"popularity.desc"},{name:"Latest", value:"primary_release_date.desc"},{name:"Top Rated", value:"vote_average.desc"},{name:"Blockbusters", value:"revenue.desc"}]
-  
-  return (<Grid templateAreas={{
+  const l = [{ name: null, value: null }, { name: "Popularity", value: "popularity.desc" }, { name: "Latest", value: "primary_release_date.desc" }, { name: "Top Rated", value: "vote_average.desc" }, { name: "Blockbusters", value: "revenue.desc" }]
+
+  return (
+  <>
+  <Grid templateAreas={{
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"`
   }}
-  templateColumns={{
-    base: '1fr',
-    lg: '200px 1fr'
-  }}
+    templateColumns={{
+      base: '1fr',
+      lg: '200px 1fr'
+    }}
   >
     <GridItem area="nav">
-      <NavBar onSearch={(search)=> setSearch(search)} searchText={searchText} />
+      <NavBar onSearch={(search) => setSearch(search)} searchText={searchText} />
 
-      
+
     </GridItem>
     <Show above="lg">
-    <GridItem area="aside" >
-      <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
-    </GridItem>
+      <GridItem area="aside" >
+        <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
+      </GridItem>
     </Show>
     <GridItem area="main">
-      <PlateformSelector list={l}  selectedFilter={selectedFilter} onSelectFilter={(filter) => setSelectedFilter(filter)}/>
-      <MovieGrid selectedGenre={selectedGenre} page={page} selectedFilter={selectedFilter} searchQuery={searchText}/>
+      <PlateformSelector list={l} selectedFilter={selectedFilter} onSelectFilter={(filter) => setSelectedFilter(filter)} />
+      <MovieGrid selectedGenre={selectedGenre} page={page} selectedFilter={selectedFilter} searchQuery={searchText} />
       <Center>
-      <PageDirection changePage={(page)=>{page > 0 && setPage(page)}} page={page}/>
+        <PageDirection changePage={(page) => { page > 0 && setPage(page) }} page={page} />
       </Center>
     </GridItem>
 
-  </Grid>)
+  </Grid>
+  <Analytics/>
+  </>
+  )
 }
- export default App
+export default App
